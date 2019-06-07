2a0f309feea52c7631d84a23a7d431134e539fca
RaquelRufino <quelrufino35@gmail.com>
2019-06-06 21:05:04 -0300

    Finishing the application

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 
 # This is a simple todo list application with MongoDB, Express, React and Node.js
+
+## Instalation
+
+To run the project locally you should run the commands above:
+
+´´´
+cd fronted
+yarn install
+yarn start
+´´´
+´´´
+cd backend
+npm install
+nodemon server
+´´´
+The `yarn start` command will open a browser window with the dashboard running on `localhost:3000`
--- frontend/src/components/todos-list.component.js
+++ frontend/src/components/todos-list.component.js
@@ -5,9 +5,9 @@ import axios from 'axios';
 
 const Todo = props => (
     <tr>
-        <td>{props.todo.todo_description}</td>
-        <td>{props.todo.todo_responsible}</td>
-        <td>{props.todo.todo_priority}</td>
+        <td className={props.todo.todo_completed ? 'completed': ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed': ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed': ''}>{props.todo.todo_priority}</td>
         <td>
             <Link to={"/edit/"+props.todo._id}>Edit</Link>
         </td>
@@ -29,6 +29,16 @@ export default class TodosList extends Component {
             .catch(function (error) {
                 console.log(error);
             })
+    }
+
+    componentDidUpdate() {
+        axios.get('http://localhost:4000/todos/')
+        .then(response => {
+            this.setState({todos: response.data});
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
     }
 
     todoList() {
